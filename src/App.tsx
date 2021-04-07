d70d3102c19b5a61b900741ac76ad309d2713999
doong2 <jungseok.kang@themoin.com>
2021-04-07 11:52:47 +0900

    svg 파일 위치이동

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Global } from '@emotion/react'
 import Header from './components/Header'
 import Routes from './Routes'
-import globalStyle from './mds/globalStyle'
+import globalStyle from './mds/theme/globalStyle'
 
 function App() {
   return (
