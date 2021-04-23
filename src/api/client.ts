094c61c52d5aa5a3befef73a42a45e62b7893fcf
doong2 <jungseok.kang@themoin.com>
2021-04-23 11:49:50 +0900

    Add: API 인터셉터 추가

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 const client = axios.create()
 
@@ -6,5 +6,17 @@ client.defaults.baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : 'https://antvile.api.com'
+
+client.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error: AxiosError) {
+    if (error.response) {
+      return Promise.reject(error.response)
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default client
