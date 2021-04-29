f48190f5ac8d5accef5d649400103295ce7455c1
doong2 <jungseok.kang@themoin.com>
2021-04-29 14:53:06 +0900

    Add: 인증 redirect 라우터

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Switch } from 'react-router'
 import { Route } from 'react-router-dom'
+import AuthRoute from './components/Route/AuthRoute'
+import useCheckLogin from './hooks/useCheckLogin'
 import FeedPage from './pages/FeedPage'
 import LandingPage from './pages/LandingPage'
 
 const Routes = () => {
+  const authenticated = useCheckLogin()
   return (
     <Switch>
-      <Route exact path={['/', 'landing']} component={LandingPage} />
+      <AuthRoute
+        exact
+        path={['/', 'landing']}
+        authenticated={authenticated}
+        component={LandingPage}
+      />
       <Route exact path="/feed" component={FeedPage} />
     </Switch>
   )
