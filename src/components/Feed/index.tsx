0322487d3812403d6d1b368ae3a5801bb5f8b8eb
doong2 <jungseok.kang@themoin.com>
2021-05-03 15:53:40 +0900

    Add: 사이드바 마크업

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,7 +1,33 @@
+import styled from '@emotion/styled'
 import React from 'react'
+import SideBar from '../SideBar'
+
+const Wrapper = styled.div`
+  min-width: 144rem;
+`
+
+const BarWrapper = styled.div`
+  width: 144rem;
+  padding: 3rem 0;
+  margin: 0 auto;
+  position: relative;
+`
+
+const FeedWrapper = styled.div`
+  width: 68.4rem;
+  margin: 0 auto;
+`
 
 const Feed = () => {
-  return <></>
+  return (
+    <Wrapper>
+      <BarWrapper>
+        <FeedWrapper>
+          <SideBar />
+        </FeedWrapper>
+      </BarWrapper>
+    </Wrapper>
+  )
 }
 
 export default Feed
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,92 @@
 import styled from '@emotion/styled'
+import { grey040 } from '../../mds/theme/colors'
 
-const MainContainer = styled.div``
+const Wrapper = styled.div`
+  position: absolute;
+  left: 2.4rem;
+`
+
+const Card = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 4px 14px rgba(32, 32, 32, 0.12),
+    0px 1.6711px 5.84887px rgba(32, 32, 32, 0.0862625),
+    0px 0.893452px 3.12708px rgba(32, 32, 32, 0.0715329),
+    0px 0.500862px 1.75302px rgba(32, 32, 32, 0.06),
+    0px 0.266004px 0.931014px rgba(32, 32, 32, 0.0484671),
+    0px 0.11069px 0.387416px rgba(32, 32, 32, 0.0337375);
+`
+
+const Title = styled.div`
+  display: flex;
+  width: 29.7rem;
+  padding: 1rem 1.2rem;
+  height: 4.1rem;
+
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 18px;
+
+  color: #202020;
+
+  border-bottom: 0.5px solid #1942e0;
+`
+
+const Main = styled.div`
+  height: 25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const MainLabel = styled.div`
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 150%;
+
+  text-align: center;
+
+  color: #202020;
+`
+
+const Footer = styled.div`
+  margin-top: 2rem;
+`
+
+const Group = styled.div`
+  display: flex;
+  column-gap: 1rem;
+`
+
+const Item = styled.div`
+  color: ${grey040};
+
+  font-weight: 500;
+  font-size: 1.3rem;
+  line-height: 1.8rem;
+`
 
 function SideBar() {
   return (
     <>
-      <MainContainer></MainContainer>
+      <Wrapper>
+        <Card>
+          <Title>관심 종목</Title>
+          <Main>
+            <MainLabel>
+              원하는 종목을 검색하고
+              <br />
+              관심 종목 리스트에 등록해보세요!
+            </MainLabel>
+          </Main>
+        </Card>
+        <Footer>
+          <Group>
+            <Item>약관</Item>
+            <Item>개인정보 처리 방침</Item>
+            <Item>© 2021 Antville, Inc.</Item>
+          </Group>
+        </Footer>
+      </Wrapper>
     </>
   )
 }
