eb4195c878f9063bb5b57c09080bdab2aa92c7cd
doong2 <jungseok.kang@themoin.com>
2021-05-06 11:51:52 +0900

    Update: svg 속성 이름 수정

--- src/assets/svg/PictureUploadButton.tsx
+++ src/assets/svg/PictureUploadButton.tsx
@@ -6,14 +6,14 @@ function PictureUploadButton(props: any) {
       <path
         d="M23.625 5.25H4.375a.875.875 0 00-.875.875v15.75c0 .483.392.875.875.875h19.25a.875.875 0 00.875-.875V6.125a.875.875 0 00-.875-.875z"
         stroke="#1942E0"
-        stroke-width="1.1"
+        strokeWidth={1.1}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M3.5 18.376l5.506-5.506a.875.875 0 011.238 0l4.887 4.887a.875.875 0 001.238 0l2.262-2.262a.875.875 0 011.238 0l4.631 4.63"
         stroke="#1942E0"
-        stroke-width="1.1"
+        strokeWidth={1.1}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
