5ef76921f827a8e61bfe60ba84deeed3678d8f79
doong2 <jungseok.kang@themoin.com>
2021-05-12 10:32:18 +0900

    Add: mds 컬러 추가

--- src/mds/styled/colors.ts
+++ src/mds/styled/colors.ts
@@ -1,8 +1,15 @@
+export const antpink050 = '#FA1D65'
+
+export const antblue030 = '#6f7deb'
+export const antblue040 = '#4b5fe7'
+export const antblue050 = '#1942EO'
+
 export const navy030 = '#6f7deb'
 export const navy040 = '#4b5fe7'
 export const navy050 = '#1942E0'
 
 export const red010 = '#FFF8F8'
+export const red020 = '#FFF0F0'
 export const red050 = '#FF3F3E'
 
 export const pink050 = '#FA4A61'
@@ -10,6 +17,11 @@ export const pink050 = '#FA4A61'
 export const blue010 = '#F6FBFF'
 export const blue040 = '#4C9EEB'
 export const blue050 = '#3082F5'
+
+export const sky010 = '#F6FBFF'
+export const sky020 = '#E4EFFF'
+export const sky040 = '#4C9EEB'
+export const sky050 = '#3082F5'
 
 export const grey010 = '#FAFAFA'
 export const grey020 = '#F4F4F4'
